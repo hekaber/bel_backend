e28d3534c8d7d9ae5b194e7145d13c84b34af232
Karl <hkberger@gmail.com>
2023-01-15 15:59:07 +0100

    [TASK] Refactor dependencies

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
 from .routers import home, contracts, items
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 app = FastAPI()
+
 app.include_router(contracts.router)
 app.include_router(items.router)
 app.include_router(home.router)
--- app/routers/home.py
+++ app/routers/home.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter
-
+from fastapi import Depends, APIRouter
+from ..dependencies import oauth2
 
 router = APIRouter(
     prefix="",
     tags=[""],
-    dependencies=[],
     responses={404: {"description": "Not found"}},
 )
 
 @router.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root(token: str = Depends(oauth2.oauth2_scheme)):
+    return {"token": token }
+
+@router.get("/testing")
+async def read_testing(token: str = Depends(oauth2.oauth2_scheme)):
+    return {"token": token, "status": "test" }
