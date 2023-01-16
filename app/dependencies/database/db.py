c144712936bc38caeabf7be8831d28274cac1d1f
Karl <hkberger@gmail.com>
2023-01-16 10:58:36 +0100

    [TASK] Set connection to database

---
+++ app/dependencies/database/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# TODO: set it with env variables
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://bel:1234@beldb/local"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
