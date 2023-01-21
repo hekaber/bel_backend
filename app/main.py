cd14ef5a97532d03128e0fc32b655706099c0df6
Henry Karl Berger <hkberger@gmail.com>
2023-01-22 00:21:47 +0100

    [TASK] Remove items

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import home, contracts, items, users
+from .routers import home, contracts, items, users, certifications
 from .dependencies.database.db import engine
 from .dependencies.database.base import Base
 from .classes.config import load_env
@@ -9,6 +9,6 @@ load_env()
 app = FastAPI()
 
 app.include_router(contracts.router)
-app.include_router(items.router)
 app.include_router(home.router)
 app.include_router(users.router)
+app.include_router(certifications.router)
